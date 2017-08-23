a8135904acc6eaf6de092541cbc05f3cd4582f3d
Akash Nagwanshi <akashnagwanshi@gmail.com>
2017-08-23 17:32:37 +0530

    Create aditi_ka_test.c

---
+++ aditi_ka_test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+char ch[50];
+
+
+
+    printf("Aditi{nanhi didi} me ego he ki nahi yes|no.....\n");
+  
+    scanf("%s",&ch);
+  //  printf("Aditi{nanhi didi} me ego he ki nahi yes|no.....\n");
+    if(strcmp(ch,"yes")==0){
+    printf("\nfinally you accepted ....\nconclusion \nADITI+STIC = 'EGOISTIC' \n");
+    printf(" ");
+     exit(0);
+    }
+    else
+    printf(" ummmm... not possible ....try again....\n");
+    main();
+    
+    
+return 0;
+}
